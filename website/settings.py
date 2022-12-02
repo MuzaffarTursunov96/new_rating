bac76730442b30a579c0eb4bc2fc4107ae27355f
MuzaffarTusunov96 <Muzaffar.Tursunov102196@gmail.com>
2022-12-02 17:36:08 +0300

    sdsd

--- website/settings.py
+++ website/settings.py
@@ -78,10 +78,20 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testowa',
+        'USER': 'testowy',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
